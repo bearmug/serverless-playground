1eadb166329be2232dcd297bb075b76a45d65551
Pavel Fadeev <pavel.fadeev@gmail.com>
2019-11-08 19:52:04 +0100

    #12 upgrade all gradle build files

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    groovy
+    application
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+apply(from = "config/serverless/serverless.gradle.kts")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+version = "0.1"
+group = "bearmug.lambda"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://jcenter.bintray.com")
+}
+
+val micronautVersion: String by project
+dependencies {
+    annotationProcessor(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    annotationProcessor("io.micronaut:micronaut-graal:$micronautVersion")
+    annotationProcessor("org.projectlombok:lombok:1.18.10")
+    annotationProcessor("io.micronaut:micronaut-inject-java:$micronautVersion")
+    compileOnly("com.oracle.substratevm:svm")
+    compileOnly("org.projectlombok:lombok:1.18.10")
+    implementation(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    implementation("io.micronaut:micronaut-runtime:$micronautVersion")
+    implementation("io.projectreactor:reactor-core")
+    implementation("io.micronaut.aws:micronaut-function-aws-custom-runtime:$micronautVersion") {
+        exclude(group = "com.fasterxml.jackson.module", module = "jackson-module-afterburner")
+    }
+    runtimeOnly("io.micronaut:micronaut-http-server-netty:$micronautVersion")
+    runtimeOnly("ch.qos.logback:logback-classic:1.3.0-alpha4")
+    testCompile("io.micronaut:micronaut-http-server-netty:$micronautVersion")
+    testImplementation("io.micronaut.test:micronaut-test-spock:$micronautVersion")
+    testImplementation("io.micronaut:micronaut-inject-groovy:$micronautVersion")
+    testImplementation("org.spockframework:spock-core") {
+        exclude(group = "org.codehaus.groovy", module = "groovy-all")
+    }
+}
+
+application {
+    mainClassName = "bearmug.lambda.ApplicationGCP"
+}
+
+tasks {
+    register<Zip>("buildZip") {
+        from(sourceSets.main.get().output)
+        into("lib") { from(
+            configurations.compileClasspath,
+            configurations.runtimeClasspath)
+        }
+    }
+
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        dependsOn += "buildZip"
+    }
+
+    named<JavaExec>("run") {
+        doFirst {
+            jvmArgs = listOf("-noverify", "-XX:TieredStopAtLevel=1", "-Dcom.sun.management.jmxremote")
+        }
+    }
+
+    withType<JavaCompile>{
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
+    }
+}
+
--- config/serverless/serverless.gradle.kts
+++ config/serverless/serverless.gradle.kts
@@ -1,101 +1,103 @@
-//GCP tasks=====================================================================
-val gcpTag = "gcp-serverless-graal"
-tasks.create<Exec>("gcpAssemble") {
-    group = "serverless"
-    inputs.files(
-            project.fileTree("src"),
-            file("config/graal/Dockerfile-gcp-cloudrun"),
-            file("build.gradle"))
-    outputs.file("build/$gcpTag-version")
-    commandLine("bash", "-c",
-            "docker build . " +
-                    "-t $gcpTag " +
-                    "-t eu.gcr.io/gcp-serverless-257812/$gcpTag " +
-                    "-f $rootDir/config/graal/Dockerfile-gcp-cloudrun && " +
-            "docker images -q $gcpTag > build/$gcpTag-version")
-}
-
-tasks.create<Exec>("gcpDeploy") {
-    group = "serverless"
-    dependsOn("gcpAssemble")
-    commandLine("bash", "-c",
-            "docker push eu.gcr.io/gcp-serverless-257812/$gcpTag && " +
-            "gcloud beta run deploy " +
-                    "$gcpTag " +
-                    "--platform=managed " +
-                    "--allow-unauthenticated " +
-                    "--region=europe-west1 " +
-                    "--max-instances=1 " +
-                    "--concurrency=10 " +
-                    "--image=eu.gcr.io/gcp-serverless-257812/$gcpTag")
-}
-
-tasks.register<Exec>("gcpStartLocal") {
-    group = "serverless"
-    dependsOn("gcpAssemble")
-    commandLine("bash", "-c",
-            "docker run --rm --name $gcpTag -p 8080:8080 -d $gcpTag &&  docker ps")
-}
-
-tasks.register<Exec>("gcpStopLocal") {
-    group = "serverless"
-    commandLine("bash", "-c",
-            "docker kill $gcpTag && docker ps")
-}
-
-////AWS tasks=====================================================================
-tasks.register<Exec>("awsLambdaAssemble"){
-    group = "serverless"
-    inputs.files(
-            project.fileTree("src"),
-            file("config/graal/Dockerfile-aws-lambda"),
-            file("build.gradle"))
-    outputs.file("build/function.zip")
-    val tag = "serverless-graal"
-    commandLine("bash", "-c",
-            "docker build . -t $tag -f $rootDir/config/graal/Dockerfile-aws-lambda && " +
-            "mkdir -p build && " +
-            "docker run --rm --entrypoint cat $tag  /home/application/function.zip > build/function.zip")
-}
-
-tasks.register<Exec>("awsLambdaDeploy"){
-    group = "serverless"
-    dependsOn("build", "awsLambdaAssemble")
-    commandLine("bash", "-c", "serverless deploy")
-}
-
-tasks.register<Exec>("awsLambdaRemove") {
-    group = "serverless"
-    commandLine("bash", "-c", "serverless remove")
-}
-
-tasks.register<Exec>("awsLambdaStatsJava") {
-    group = "serverless"
-    val fn = "-f serverless-mn-java"
-    commandLine("bash", "-c",
-            "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
-}
-
-tasks.register<Exec>("awsLambdaStatsGraal") {
-    group = "serverless"
-    val fn = "-f serverless-mn-graal"
-    commandLine("bash", "-c",
-            "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
-}
-
-tasks.register<Exec>("awsLambdaStatsNode") {
-    group = "serverless"
-    val fn = "-f serverless-nodejs"
-    commandLine("bash", "-c",
-            "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
-}
-
-////K6 tasks=====================================================================
-val url =  project.findProperty("urlArg")
-tasks.register<Exec>("loadTest") {
-    doFirst {
-        checkNotNull(url) { "Please set URL to run load test against: -PurlArg=<your-url-here>" }
+tasks {
+    //GCP tasks=====================================================================
+    val gcpTag = "gcp-serverless-graal"
+    register<Exec>("gcpAssemble") {
+        group = "serverless"
+        inputs.files(
+                project.fileTree("src"),
+                file("config/graal/Dockerfile-gcp-cloudrun"),
+                file("build.gradle"))
+        outputs.file("build/$gcpTag-version")
+        commandLine("bash", "-c",
+                "docker build . " +
+                        "-t $gcpTag " +
+                        "-t eu.gcr.io/gcp-serverless-257812/$gcpTag " +
+                        "-f $rootDir/config/graal/Dockerfile-gcp-cloudrun && " +
+                        "docker images -q $gcpTag > build/$gcpTag-version")
     }
-    commandLine("bash", "-c",
-            "docker run -i loadimpact/k6 run -e url=$url -< $rootDir/config/load-test/load-test-simple.js")
+
+    register<Exec>("gcpDeploy") {
+        group = "serverless"
+        dependsOn("gcpAssemble")
+        commandLine("bash", "-c",
+                "docker push eu.gcr.io/gcp-serverless-257812/$gcpTag && " +
+                        "gcloud beta run deploy " +
+                        "$gcpTag " +
+                        "--platform=managed " +
+                        "--allow-unauthenticated " +
+                        "--region=europe-west1 " +
+                        "--max-instances=1 " +
+                        "--concurrency=10 " +
+                        "--image=eu.gcr.io/gcp-serverless-257812/$gcpTag")
+    }
+
+    register<Exec>("gcpStartLocal") {
+        group = "serverless"
+        dependsOn("gcpAssemble")
+        commandLine("bash", "-c",
+                "docker run --rm --name $gcpTag -p 8080:8080 -d $gcpTag &&  docker ps")
+    }
+
+    register<Exec>("gcpStopLocal") {
+        group = "serverless"
+        commandLine("bash", "-c",
+                "docker kill $gcpTag && docker ps")
+    }
+
+    ////AWS tasks=====================================================================
+    register<Exec>("awsLambdaAssemble") {
+        group = "serverless"
+        inputs.files(
+                project.fileTree("src"),
+                file("config/graal/Dockerfile-aws-lambda"),
+                file("build.gradle"))
+        outputs.file("build/function.zip")
+        val tag = "serverless-graal"
+        commandLine("bash", "-c",
+                "docker build . -t $tag -f $rootDir/config/graal/Dockerfile-aws-lambda && " +
+                        "mkdir -p build && " +
+                        "docker run --rm --entrypoint cat $tag  /home/application/function.zip > build/function.zip")
+    }
+
+    register<Exec>("awsLambdaDeploy") {
+        group = "serverless"
+        dependsOn("build", "awsLambdaAssemble")
+        commandLine("bash", "-c", "serverless deploy")
+    }
+
+    register<Exec>("awsLambdaRemove") {
+        group = "serverless"
+        commandLine("bash", "-c", "serverless remove")
+    }
+
+    register<Exec>("awsLambdaStatsJava") {
+        group = "serverless"
+        val fn = "-f serverless-mn-java"
+        commandLine("bash", "-c",
+                "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
+    }
+
+    register<Exec>("awsLambdaStatsGraal") {
+        group = "serverless"
+        val fn = "-f serverless-mn-graal"
+        commandLine("bash", "-c",
+                "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
+    }
+
+    register<Exec>("awsLambdaStatsNode") {
+        group = "serverless"
+        val fn = "-f serverless-nodejs"
+        commandLine("bash", "-c",
+                "serverless logs $fn  && serverless info $fn && serverless metrics $fn")
+    }
+
+    ////K6 tasks=====================================================================
+    val url = project.findProperty("urlArg")
+    register<Exec>("loadTest") {
+        doFirst {
+            checkNotNull(url) { "Please set URL to run load test against: -PurlArg=<your-url-here>" }
+        }
+        commandLine("bash", "-c",
+                "docker run -i loadimpact/k6 run -e url=$url -< $rootDir/config/load-test/load-test-simple.js")
+    }
 }
